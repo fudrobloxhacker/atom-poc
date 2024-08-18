a597610cfb854d71d87cebc38051483ce7328a8f
fudrobloxhacker <92699085+fudrobloxhacker@users.noreply.github.com>
2024-08-18 05:03:47 -0500

    forgot to loadlibrary

--- files/atom.cpp
+++ files/atom.cpp
@@ -4,11 +4,13 @@
 
 int main() {
 
+    LoadLibrary(L"User32.dll");
+
     for (int i = 0xC000; i < 0xFFFF; ++i) {
-        wchar_t buffer[256] = {0};
+        wchar_t buffer[256] = { 0 };
 
         if (GlobalGetAtomName(i, buffer, ARRAYSIZE(buffer))) {
-            wprintf(L"Atom : %s\n", buffer);
+            wprintf(L"%s\n", buffer);
         }
     }
 
